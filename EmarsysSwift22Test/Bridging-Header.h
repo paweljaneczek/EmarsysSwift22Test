ad53889699af2244c1dbeb3e09a50873e2bf9424
Paweł Janeczek <pawel.janeczek@polidea.com>
2016-09-28 10:51:27 +0200

    first commit

---
+++ EmarsysSwift22Test/Bridging-Header.h
@@ -0,0 +1,1 @@
+#import <EmarsysPushSDK/EmarsysPushSDK.h>
---
+++ EmarsysSwift22Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmarsysSwift22Test
+//
+//  Created by Paweł Janeczek on 28.09.2016.
+//  Copyright © 2016 Showroom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
